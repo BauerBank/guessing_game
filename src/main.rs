56ced8efc7b060fc6efef1e624643db83eef87e5
BauerBank <94458982+BauerBank@users.noreply.github.com>
2022-12-07 19:40:47 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+//First Part done weiter mit Generating a Secret Number 2.
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+}
