4b5bf2954c6ba999b68e645a47458ba48e1c4f2b
BauerBank <94458982+BauerBank@users.noreply.github.com>
2022-12-26 17:45:20 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 //First Part done weiter mit Generating a Secret Number 2.
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = 
+    rand::thread_rng().gen_range(1..=100);
+
+    loop {
 
     println!("Please input your guess.");
 
@@ -13,5 +20,23 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+        let guess: u32 = 
+        match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+        };
+    
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => { 
+            println!("You win!");
+            break;
+        }
+        
+    }
+
+}
 }
